b6ff2d44188ebefae19312ddf843bde46024b566
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 16:39:26 -0700

    working restaurant ratings dictionary

---
+++ restaurant-ratings.py
@@ -0,0 +1,22 @@
+# your code goes here
+# Your job is to write a program named 
+# restaurant-ratings.py that reads the file, 
+# then spits out the ratings in alphabetical 
+# order by restaurant.
+
+restaurant_ratings = {}
+
+# restaurant1 = "Arinell's:4"
+# restaurant1 = restaurant1.split(":")
+# restaurant_ratings["Arinell's"] = 4
+# print restaurant_ratings
+
+
+scores = open("scores.txt")
+
+for line in scores:
+    line = line.rstrip()
+    line = line.split(":")
+    # print line
+    restaurant_ratings[line[0]] = line[1]
+print restaurant_ratings
